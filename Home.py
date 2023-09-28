5ad3f2451878c854c9e4a18302da68d5285fac56
Juan Diaz Suarez <jdiazsua@redhat.com>
2023-09-28 12:55:45 +0200

    add info to home page

--- Home.py
+++ Home.py
@@ -12,3 +12,17 @@ st.set_page_config(
 )
 
 st.title('Friendlier Jira')
+
+st.markdown(
+    """
+Welcome to the Friendlier Jira tool. A webapp that lets you run SQL
+queries on your favourite Jira instance, be it cloud based or self-hosted.
+
+Use the menu on the left side of the page to navigate. You can run custom
+queries, build custom dashboard or use the prebuilt ones.
+""")
+
+st.image(
+    "https://thenounproject.com/api/private/icons/546312/edit/?backgroundShape=SQUARE&backgroundShapeColor=%23000000&backgroundShapeOpacity=0&exportSize=752&flipX=false&flipY=false&foregroundColor=%23000000&foregroundOpacity=1&imageFormat=png&rotation=0",
+    width=250
+)
