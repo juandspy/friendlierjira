f12e6c17cb2ef0395a7da5e48ea9cb1e88865fe8
Juan Diaz Suarez <jdiazsua@redhat.com>
2023-09-28 12:41:50 +0200

    start page in wide mode

--- Home.py
+++ Home.py
@@ -3,4 +3,12 @@
 import streamlit as st
 
 
+st.set_page_config(
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Report a bug': "https://github.com/juandspy/friendlierjira"
+    }
+)
+
 st.title('Friendlier Jira')
